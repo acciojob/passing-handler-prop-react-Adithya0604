6233fa1589c4d52df25600cc8a5ccc276df92b20
S.Adithya Sharma <120710617+Adithya0604@users.noreply.github.com>
2025-07-15 23:56:52 +0530

    Update Selection.js

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,14 @@
+import React from 'react';
 
+function Selection(){
+  const [BGChange, setBGCHANGE] = useState({background: ''});
+
+  const updateSelectionStyle = (style) => {setBGCHANGE(style)};
+
+  return(
+    <>
+    <div onclick={{applyColor(updateSelectionStyle)}}></div>
+    </>)
+};
+
+export default Selection
